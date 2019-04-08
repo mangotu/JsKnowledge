8c6c9f249d5c144e9734bbf4a79e9984608dbe4e
mangotu <774845209@qq.com>
2019-04-08 18:12:18 +0800

    js

--- js/promise/简单promise.js
+++ js/promise/简单promise.js
@@ -5,24 +5,24 @@ function Promise(fn){
 	that.reason = undefined; //promise 失败状态的 参数
     
 	function resolve(value){//成功
-		that.status = "fulfilled";
-		that.value = value;
+		that.status = "fulfilled";//更改 promise 状态
+		that.value = value; //缓存参数
 	}
      
 	function reject(reason){//成功
-		that.status = "rejected";
-		that.reason = reason;
+		that.status = "rejected";//更改 promise 状态
+		that.reason = reason;//缓存参数
 	}
 
-	fn(resolve,reject);
+	fn(resolve,reject); //执行器执行
 }
 
 Promise.prototype.then = function(onfulfilled,onrejected){
-	if(this.status === "fulfilled"){
-		onfulfilled(this.value);
+	if(this.status === "fulfilled"){ // 成功状态
+		onfulfilled(this.value); 
 	}
 
-	if(this.status === "rejected"){
+	if(this.status === "rejected"){ // 失败状态
 		onfulfilled(this.reason);
 	}
 
