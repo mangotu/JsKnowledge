a8ffc30ac0e5d1f3a1159bf5f8aeea6f51d42f25
涂成林 <tuchenglin@tuchenglindeMacBook-Pro.local>
2019-04-09 00:29:33 +0800

    deep clone

--- js/拷贝相关.js
+++ js/拷贝相关.js
@@ -1,3 +1,43 @@
+
 //浅拷贝
+//Object.assign()
 
 //深拷贝
+// JSON.parse(JSON.stringify)
+// 递归
+
+var a = {
+    a :1,
+    c:{
+        a:{
+            a :2
+        }
+    }
+}
+
+// var b = JSON.parse(JSON.stringify(a))
+// a.a = 2
+// console.log(b)
+
+function deepClone(obj,result){
+    var result = result || {};
+        for(var item in obj){
+            if(obj.hasOwnProperty(item)){
+                if(typeof obj[item] === "object"){
+                    if(obj[item] instanceof Array){
+                      result[item] = deepClone(obj[item],[])
+                    }else{
+                      result[item] = deepClone(obj[item])
+                    }
+                }else{
+                    result[item] = obj[item]
+                }
+            }
+        }
+    return result;
+}
+
+var c = deepClone(a)
+a.c.a.a = 11;
+
+console.log(c)
